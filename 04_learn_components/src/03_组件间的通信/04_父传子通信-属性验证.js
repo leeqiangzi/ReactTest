a9dd1f11c38d8464a15d1fc13faac7fdf8f49e7e
lizhiqiang17 <lizhiqiang17@58.com>
2022-05-03 10:52:14 +0800

    第六天：类组件和函数组件属性传值

--- 04_learn_components/src/03_组件间的通信/04_父传子通信-属性验证.js
+++ 04_learn_components/src/03_组件间的通信/04_父传子通信-属性验证.js
@@ -1,15 +1,35 @@
 import React, {Component} from 'react'
 import PropTypes from 'prop-types'
 
-function ChildCpn(props) {
-    const {name = ''} = props;
-    return (
-        <h2>我是：{name}</h2>
-    );
-}
+// function ChildCpn(props) {
+//     const {name = ''} = props;
+//     return (
+//         <h2>我是：{name}</h2>
+//     );
+// }
+//
+// ChildCpn.propType = {
+//     name: PropTypes.string,
+// }
+//
+// ChildCpn.defaultProps = {
+//     name: '',
+// }
 
-ChildCpn.prototype = {
-    name: PropTypes.string,
+class ChildCpn extends Component {
+    static propsType = {
+        name: PropTypes.string,
+    }
+
+    static defaultProps = {
+        name: ''
+    }
+
+    render() {
+        return (
+            <h2>我是：{this.props.name}</h2>
+        );
+    }
 }
 
 // 类组件
