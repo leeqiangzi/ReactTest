bc74a29fb2f532364731a0241b8cfbeb6d872d98
lizhiqiang17 <lizhiqiang17@58.com>
2022-05-09 10:36:57 +0800

    第十一天：Fragments

---
+++ 04_learn_components/src/12_组件其他内容补充/03_fragments的使用.js
@@ -0,0 +1,51 @@
+import React, {PureComponent, Fragment} from "react";
+
+export default class App extends PureComponent {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            counter: 0,
+            friends: [
+                {name: 'why', age: 18},
+                {name: 'lilei', age: 20},
+                {name: 'qiangzi', age: 28},
+            ]
+        }
+    }
+
+    render() {
+        return (
+            /**
+             <Fragment>
+             <h2>当前计数：{this.state.counter}</h2>
+             <button onClick={e => this.increment()}>+1</button>
+             </Fragment>
+             */
+
+            <>
+                <h2>当前计数：{this.state.counter}</h2>
+                <button onClick={e => this.increment()}>+1</button>
+                <div>
+                    {
+                        this.state.friends.map((item, index) => {
+                            return (
+                                <Fragment key={item.name}>
+                                    <div>{item.name}</div>
+                                    <p>{item.age}</p>
+                                    <hr/>
+                                </Fragment>
+                            );
+                        })
+                    }
+                </div>
+            </>
+        );
+    }
+
+    increment() {
+        this.setState({
+            counter: this.state.counter + 1,
+        });
+    }
+}
